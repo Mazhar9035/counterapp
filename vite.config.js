37777817199f8d08a911d4d6ba2f4fb8841da37e
Mazhar9035 <mdmazhargiyaraghar@gmail.com>
2025-07-12 13:28:09 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: "/counterapp/",
+// });
+
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
 export default defineConfig({
+  base: "/counterapp/", // ← this must match your repo name exactly
   plugins: [react()],
-})
+});
